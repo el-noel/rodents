b658871bc895027bef157b5650cf77548ee3f5df
Suhani Patel <sjp269@cornell.edu>
2024-03-18 19:17:44 -0400

    filters added

--- backend/app.py
+++ backend/app.py
@@ -1,46 +1,37 @@
-import json
 import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
-
-# ROOT_PATH for linking with all your files. 
-# Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
-
-# Get the directory of the current script
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
-
-# Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
 
 app = Flask(__name__)
 CORS(app)
 
-# Sample search using json with pandas
-def json_search(query):
-    matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
+# Load the CSV file into a dataframe
+csv_file_path = 'data/semicleanedbg.csv'
+data_df = pd.read_csv(csv_file_path)
+
+# Assuming your CSV has columns like 'title', 'description', and 'imdb_rating'
+# Adjust the column names as needed to match your CSV structure
+def csv_search(query, min_age, max_age):
+    matches = data_df[data_df['name'].str.lower().str.contains(query.lower())]
+    if min_age is not None:
+        matches = matches[matches['minage'] >= min_age]
+    if max_age is not None:
+        matches = matches[matches['minage'] <= max_age]
+    matches_filtered = matches[['name', 'description', 'average']]  # Update column names as necessary
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
 @app.route("/")
 def home():
-    return render_template('base.html',title="sample html")
+    return render_template('base.html', title="sample html")
 
 @app.route("/episodes")
 def episodes_search():
     text = request.args.get("title")
-    return json_search(text)
+    min_age = request.args.get("min_age", type=int)  # Set default as None if not provided
+    max_age = request.args.get("max_age", type=int)  # Set default as None if not provided
+    return csv_search(text, min_age, max_age)
 
-if 'DB_NAME' not in os.environ:
-    app.run(debug=True,host="0.0.0.0",port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
