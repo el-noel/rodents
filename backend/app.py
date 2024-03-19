0d06c2cbc8c997d405c2b66aad908d97ee415e18
Suhani Patel <sjp269@cornell.edu>
2024-03-18 20:13:48 -0400

    added all filters

--- backend/app.py
+++ backend/app.py
@@ -12,12 +12,16 @@ data_df = pd.read_csv(csv_file_path)
 
 # Assuming your CSV has columns like 'title', 'description', and 'imdb_rating'
 # Adjust the column names as needed to match your CSV structure
-def csv_search(query, min_age, max_age):
+def csv_search(query, min_age, min_players, max_players, category):
     matches = data_df[data_df['name'].str.lower().str.contains(query.lower())]
     if min_age is not None:
         matches = matches[matches['minage'] >= min_age]
-    if max_age is not None:
-        matches = matches[matches['minage'] <= max_age]
+    if min_players is not None:
+        matches = matches[matches['minplayers'] >= min_players]
+    if max_players is not None:
+        matches = matches[matches['maxplayers'] <= max_players]
+    if category:  # If a category is provided, filter by it
+        matches = matches[matches['boardgamecategory'].str.contains(category, case=False, na=False)]
     matches_filtered = matches[['name', 'description', 'average']]  # Update column names as necessary
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
@@ -29,9 +33,11 @@ def home():
 @app.route("/episodes")
 def episodes_search():
     text = request.args.get("title")
-    min_age = request.args.get("min_age", type=int)  # Set default as None if not provided
-    max_age = request.args.get("max_age", type=int)  # Set default as None if not provided
-    return csv_search(text, min_age, max_age)
+    min_age = request.args.get("min_age", type=int)
+    min_players = request.args.get("min_players", type=int)
+    max_players = request.args.get("max_players", type=int)
+    category = request.args.get("category")  # No type, because it's a string
+    return csv_search(text, min_age, min_players, max_players, category)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
