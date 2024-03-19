bbd5cd46c9e9a1dd23a4bad6491e8314c400d55e
Ely Noel <jnoel.el@gmail.com>
2024-03-18 21:33:09 -0400

    added boardgame about page, that appears on result click.

--- backend/app.py
+++ backend/app.py
@@ -20,9 +20,10 @@ def csv_search(query, min_age, min_players, max_players, category):
         matches = matches[matches['minplayers'] >= min_players]
     if max_players is not None:
         matches = matches[matches['maxplayers'] <= max_players]
-    if category:  # If a category is provided, filter by it
+    if category:  # If a category is provided, filter by itm
         matches = matches[matches['boardgamecategory'].str.contains(category, case=False, na=False)]
-    matches_filtered = matches[['name', 'description', 'average']]  # Update column names as necessary
+        
+    matches_filtered = matches[['name', 'description', 'average', 'objectid']]  # Update column names as necessary
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
@@ -38,6 +39,18 @@ def episodes_search():
     max_players = request.args.get("max_players", type=int)
     category = request.args.get("category")  # No type, because it's a string
     return csv_search(text, min_age, min_players, max_players, category)
+
+@app.route("/about/<game_id>")
+def about(game_id):
+    game_id = int(game_id)
+    game_details_query = data_df[data_df['objectid'] == game_id]
+    # Check if the query returned any rows
+    if not game_details_query.empty:
+        game_details = game_details_query.iloc[0].to_dict()
+        return render_template('about.html', game=game_details)
+    else:
+        # If no rows are returned, handle the case (e.g., show a not found page or message)
+        return "Game not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
