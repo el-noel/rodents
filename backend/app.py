16abf7bcfa9fd13bbe9d855951889fca6961e10f
ky322 <kellyy.aot@gmail.com>
2024-03-22 15:08:16 -0400

    get board game images

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,9 @@ import html
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 CORS(app)
 
@@ -101,9 +104,39 @@ def about(game_id):
     game_details_query = data_df[data_df['objectid'] == game_id]
     if not game_details_query.empty:
         game_details = game_details_query.iloc[0].to_dict()
+        print("!!!! LOOK HERE !!!!")
+        print(game_details)
+        # game_img = fetch_game_link(game_details_query["gamelink"])
+        print(game_id)
+        game_img = fetch_game_link(game_details_query.reset_index(drop=True).at[0, "gamelink"])
+        print("!!!! LOOK HERE !!!!")
+        print(game_img)
+        game_details["img"] = game_img
         return render_template('about.html', game=game_details)
     else:
         return "Game not found", 404
+    
+def fetch_game_link(game_link):
+    # game_link = game_link[game_link.index("/"):]
+    print("!!!! LOOK HERE !!!!")
+    print(game_link)
+    game_url = f"https://boardgamegeek.com{game_link}"
+    print("!!!! LOOK HERE !!!!")
+    print(game_link)
+    response = requests.get(game_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        img_tag = soup.find('meta', attrs={"property": "og:image"})
 
+        if img_tag:
+            return img_tag['content']
+            # if img_tag and 'src' in img_tag.attrs:
+            #     return img_tag['src']
+            # else:
+            #     return "No surrounding <a> tag with an href found."
+        else:
+            return "No <img> tag with the specified class found."
+    else:
+        return f"Failed to retrieve the webpage. Status code: {response.status_code}"
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
